2ef012fc131368214817237775b154ce37a7322d
Paweł Dąbrowski <paweldabrowski@zoho.com>
2020-04-26 13:21:36 +0200

    Modify Kalkulator PRO -> working withou check of additional number (isnumeric)

--- kalkulator.py
+++ kalkulator.py
@@ -2,28 +2,36 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile_kalkulator.log")
 
+def mnoz_liste(lista) : 
+    wynik = 1
+    for liczba in lista:
+        wynik = wynik * liczba
+    return wynik
+
+
 def kalkulator(dzialanie, liczba1, liczba2):
-    wynik = [None, None]
+    wynik = [None, None, None]
+    liczby = [liczba1, liczba2]
     #dzialanie = int(dzialanie)
     #liczba1 = float(liczba1)
     #liczba2 = float(liczba2)
     if dzialanie == 1:
-        wynik[0] = liczba1 + liczba2
         wynik[1] = "Dodaje"
+        odpowiedz = input("czy checsz dodać kolejną liczbe? [T/N]: ")
+        while odpowiedz != "N":
+            liczby.append(float(input("podaj kolejna")))
+            odpowiedz = input("czy checsz dodać kolejną liczbe? [T/N]: ")
+        wynik[0] = sum(liczby)          
     elif dzialanie == 2:
         wynik[0] = liczba1 - liczba2
         wynik[1] = "Odejmuje"
     elif dzialanie == 3:
         wynik[1] = "Mnoże"
-        wynik[0] = liczba1 * liczba2
-        odpowiedz = input("czy checsz domnożyc kolejną liczbe? [T/N]")
+        odpowiedz = input("czy checsz domnożyc kolejną liczbe? [T/N]: ")
         while odpowiedz != "N":
-            liczba2 = float(input("podaj kolejna"))
-            wynik[0] =wynik[0] * liczba2
-            odpowiedz = input("czy checsz domnożyc kolejną liczbe? [T/N]")
-
-            
-            
+            liczby.append(float(input("podaj kolejna")))
+            odpowiedz = input("czy checsz domnożyc kolejną liczbe? [T/N]: ")
+        wynik[0] = mnoz_liste(liczby)          
     elif dzialanie == 4:
         if liczba2 == 0:
             wynik[0] = "Nie dzieli się przez zero"
@@ -34,7 +42,7 @@ def kalkulator(dzialanie, liczba1, liczba2):
     else:
         wynik[0] = "Nie ma takiego dzialania"
         wynik[1] = "Inne dzialanie wiec nie wyszło"
-        
+    wynik[2] = liczby
     return wynik
 
 if __name__ == "__main__":
@@ -51,11 +59,30 @@ else:
     liczba1 = (input("podaj liczbe1: "))
     liczba2 = (input("podaj liczbe2: "))
 
-while: 
-if dzialanie.isnumeric == True:
-    dzialanie = int(dzialanie)
-else:
-    dzialanie = (input("Podaj działanie. 1 - Dodawanie, 2- Odejmowanie, 3 - mnozenie, 4 - dzielenie"))
+
+check = False
+while check == False:
+    if dzialanie.isnumeric() == True:
+        dzialanie = int(dzialanie)
+        check = True
+    else:
+        dzialanie = (input("Podano zły kod działania. Podaj działanie. 1 - Dodawanie, 2- Odejmowanie, 3 - mnozenie, 4 - dzielenie"))
+
+check = False
+while check == False:
+    if liczba1.isnumeric() == True:
+        liczba1 = float(liczba1)
+        check = True
+    else:
+        liczba1 = (input("Podano text zamiast Liczba1. Podaj Liczba1: "))
+
+check = False
+while check == False:
+    if liczba2.isnumeric() == True:
+        liczba2 = float(liczba2)
+        check = True
+    else:
+        liczba2 = (input("Podano text zamiast Liczba2. Podaj Liczba2: "))
 
 
 
@@ -63,5 +90,5 @@ wynik = kalkulator(dzialanie, liczba1, liczba2)
 logging.debug("Program został uruchomiony z następującymi parametrami: %s" % sys.argv[1:])
 logging.debug("%s liczby %s oraz %s" % (wynik[1], liczba1, liczba2))
 
-print(f"{wynik[1]} liczbę {liczba1} oraz {liczba2} co daje {wynik[0]}")
+print(f"{wynik[1]} liczby {wynik[2]} co daje {wynik[0]}")
 
